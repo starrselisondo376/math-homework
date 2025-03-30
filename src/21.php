5159d814d0c4c4406b0bc6ef8e6ec2233c3da6c7
starrselisondo376 <starrselisondo376@gmail.com>
2025-03-30 13:03:17 +0000

    Add 21.php

---
+++ src/21.php
@@ -0,0 +1,7 @@
+<?php
+// Example PHP code for weekly math homework problem 1
+function calculateSum($numbers) {
+    $sum = array_sum($numbers);
+    echo "The sum of the numbers is: " . $sum;
+}
+calculateSum([1, 2, 3, 4, 5]);
