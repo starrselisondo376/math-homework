34181fb3e7dae61132b203fc485fcfc19ad29049
starrselisondo376 <starrselisondo376@gmail.com>
2025-05-27 00:44:32 +0000

    Add 42.php

---
+++ src/42.php
@@ -0,0 +1,9 @@
+<?php
+// Example PHP code to calculate the area of a rectangle
+$length = 10;
+$width = 5;
+
+$area = $length * $width;
+
+echo "The area of the rectangle with length $length and width $width is: $area";
+?>
